445c14b2a46165c4ab9b33d8f87241c87997bc95
Tu Nombre <tvainstein@fi.uba.ar>
2025-08-28 02:00:59 -0300

    feat. se especifican los archivos de config para server y client en generar-compose a traves de docker volumes

--- generar-compose.sh
+++ generar-compose.sh
@@ -19,6 +19,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 YAML
@@ -33,6 +35,8 @@ for i in $(seq 1 $cantidad_clientes); do
     environment:
       - CLI_ID=$i
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
