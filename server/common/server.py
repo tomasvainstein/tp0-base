c8caa87aaf8f10a958a14d4c17dddfffdef8c6f0
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-09-02 22:13:42 +0000

    Revert "(ej6) fix. se agrega timeout en el server para que deje de esperar conexiones"
    This reverts commit 9be7e010693c7fa0f1d2c0473b797679567af36a.

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,5 @@
 import socket
 import logging
-import time
 from .communication_protocol import read_message, send_ack, parse_bet_payload, parse_bet_batch_payload, MSG_TYPE_BET
 from .utils import store_bets
 
@@ -11,8 +10,6 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self._running = True
-        self._last_connection_time = time.time()
-        self._idle_timeout = 5.0
 
     def run(self):
         """
@@ -26,27 +23,13 @@ class Server:
         logging.info('action: accept_connections | result: in_progress')
         while self._running:
             try:
-                self._server_socket.settimeout(1.0)
-
                 client_sock = self.__accept_new_connection()
                 if client_sock:
-                    self._last_connection_time = time.time()
                     self.__handle_client_connection(client_sock)
-                else:
-                    current_time = time.time()
-                    if current_time - self._last_connection_time > self._idle_timeout:
-                        logging.info(f'action: idle_timeout | result: success | timeout: {self._idle_timeout}s')
-                        break
 
-            except socket.timeout:
-                current_time = time.time()
-                if current_time - self._last_connection_time > self._idle_timeout:
-                    logging.info(f'action: idle_timeout | result: success | timeout: {self._idle_timeout}s')
-                    break
-                continue
             except Exception as e:
                 if self._running:
-                    logging.error(f"action: accept_connections | result: fail | error: {e}")
+                    logging.error("action: accept_connections | result: fail | error: {e}")
                 break
         
         self._cleanup()
@@ -127,12 +110,8 @@ class Server:
         Then connection created is printed and returned
         """
 
-        try:
-            # Connection arrived
-            logging.info('action: accept_connections | result: in_progress')
-            c, addr = self._server_socket.accept()
-            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
-            return c
-        except socket.timeout:
-            # Timeout esperado, retornar None
-            return None
+        # Connection arrived
+        logging.info('action: accept_connections | result: in_progress')
+        c, addr = self._server_socket.accept()
+        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        return c
