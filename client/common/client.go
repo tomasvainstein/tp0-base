ec06924a028772f523ddefe905f177431247c5b8
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-09-04 19:40:03 +0000

    Revert "(ej6) temp fix. se elimina ctx"
    This reverts commit d0a86f28314eec95f5b206c1a671569104c76dd6.

--- client/common/client.go
+++ client/common/client.go
@@ -1,11 +1,13 @@
 package common
 
 import (
+	"context"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/op/go-logging"
@@ -32,13 +34,21 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	mu     sync.Mutex
+	running bool
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
+	ctx, cancel := context.WithCancel(context.Background())
 	client := &Client{
 		config: config,
+		running: true,
+		ctx:    ctx,
+		cancel: cancel,
 	}
 	return client
 }
@@ -60,9 +70,26 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
-func (c *Client) Cleanup() {
-	log.Info("action: cleanup | result: in_progress")
+func (c *Client) Stop() {
+	log.Info("action: graceful_shutdown | result: in_progress")
+	
+	c.mu.Lock()
+	c.running = false
+	c.mu.Unlock()
+	
+	c.cancel()
+	
+	c.cleanup()
+	
+	log.Info("action: graceful_shutdown | result: success")
+}
 
+func (c *Client) cleanup() {
+	log.Info("action: cleanup | result: in_progress")
+	
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	
 	if c.conn != nil {
 		c.conn.Close()
 		log.Info("action: cleanup | result: success | resource: client_connection")
@@ -130,6 +157,13 @@ func (c *Client) StartClientLoop() {
 	batchCount := 0
 	
 	processBatch := func(batch []*Bet) error {
+		c.mu.Lock()
+		if !c.running {
+			c.mu.Unlock()
+			return fmt.Errorf("client stopped")
+		}
+		c.mu.Unlock()
+		
 		if batchCount >= c.config.LoopAmount {
 			return fmt.Errorf("reached maximum batch count")
 		}
@@ -156,7 +190,12 @@ func (c *Client) StartClientLoop() {
 		c.conn.Close()
 		batchCount++
 
-		time.Sleep(c.config.LoopPeriod)
+		select {
+		case <-time.After(c.config.LoopPeriod):
+		case <-c.ctx.Done():
+			log.Infof("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
+			return fmt.Errorf("context cancelled")
+		}
 		
 		return nil
 	}
@@ -167,5 +206,11 @@ func (c *Client) StartClientLoop() {
 		return
 	}
 	
-	log.Infof("action: loop_finished | result: success | client_id: %v | batches_sent: %d", c.config.ID, batchCount)
+	c.mu.Lock()
+	if c.running {
+		log.Infof("action: loop_finished | result: success | client_id: %v | batches_sent: %d", c.config.ID, batchCount)
+	} else {
+		log.Infof("action: loop_interrupted | result: success | client_id: %v | batches_sent: %d", c.config.ID, batchCount)
+	}
+	c.mu.Unlock()
 }
--- client/main.go
+++ client/main.go
@@ -140,10 +140,19 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGTERM)
 	
-	go client.StartClientLoop()
+	done := make(chan bool, 1)
 	
-	<-sigChan
-	log.Info("action: signal_received | result: success | signal: SIGTERM")
+	go func() {
+		client.StartClientLoop()
+		done <- true
+	}()
 	
-	client.Cleanup()
+	select {
+	case <-sigChan:
+		log.Info("action: signal_received | result: success | signal: SIGTERM")
+		client.Stop()
+	case <-done:
+		log.Info("action: client_finished | result: success")
+		client.Stop()
+	}
 }
