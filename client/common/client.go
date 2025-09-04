13ff902ee44ef0a0f80718eb699ef3c4931b5bfe
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-09-04 20:12:56 +0000

    (ej5) fix. saco sleep

--- client/common/client.go
+++ client/common/client.go
@@ -97,9 +97,6 @@ func (c *Client) StartClientLoop() {
 		}
 
 		c.conn.Close()
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
