c234981da3da28b9a5d3eb63a031d8de918cc174
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-09-04 19:36:01 +0000

    (ej6) temp fix. se cierran las comunicaciones

--- client/common/client.go
+++ client/common/client.go
@@ -110,6 +110,10 @@ func (c *Client) processCSVBets(processBatch func([]*Bet) error) error {
 
 		if len(currentBatch) >= c.config.BatchMaxAmount {
 			if err := processBatch(currentBatch); err != nil {
+				if err.Error() == "reached maximum batch count" {
+					log.Infof("action: batch_limit_reached | result: success | total_bets_processed: %d", totalBets)
+					return nil
+				}
 				return fmt.Errorf("error processing batch: %v", err)
 			}
 			currentBatch = nil
@@ -118,6 +122,10 @@ func (c *Client) processCSVBets(processBatch func([]*Bet) error) error {
 
 	if len(currentBatch) > 0 {
 		if err := processBatch(currentBatch); err != nil {
+			if err.Error() == "reached maximum batch count" {
+				log.Infof("action: batch_limit_reached | result: success | total_bets_processed: %d", totalBets)
+				return nil
+			}
 			return fmt.Errorf("error processing final batch: %v", err)
 		}
 	}
@@ -142,6 +150,8 @@ func (c *Client) StartClientLoop() {
 		err := c.sendBetBatch(batch)
 		if err != nil {
 			log.Errorf("action: send_batch | result: fail | client_id: %v | batch_size: %d | error: %v", c.config.ID, len(batch), err)
+			c.conn.Close()
+			return err
 		} else {
 			log.Infof("action: batch_sent | result: success | client_id: %v | batch_size: %d", c.config.ID, len(batch))
 		}
@@ -149,6 +159,8 @@ func (c *Client) StartClientLoop() {
 		err = c.getAck()
 		if err != nil {
 			log.Errorf("action: receive_ack | result: fail | client_id: %v | batch_size: %d | error: %v", c.config.ID, len(batch), err)
+			c.conn.Close()
+			return err
 		} else {
 			log.Infof("action: apuesta_enviada | result: success | client_id: %v | batch_size: %d", c.config.ID, len(batch))
 		}
