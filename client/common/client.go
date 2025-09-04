612107cdf2e708bed5bfa4b9e0f89dd590f82e11
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-09-04 05:58:34 +0000

    REVERT->(ej4) fix. se sacan el mutex y los locks innecesarios

--- client/common/client.go
+++ client/common/client.go
@@ -3,6 +3,7 @@ package common
 import (
 	"context"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/op/go-logging"
@@ -27,15 +28,21 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	mu     sync.Mutex
 	running bool
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
+	ctx, cancel := context.WithCancel(context.Background())
 	client := &Client{
 		config: config,
 		running: true,
+		ctx:    ctx,
+		cancel: cancel,
 	}
 	return client
 }
@@ -60,15 +67,22 @@ func (c *Client) createClientSocket() error {
 func (c *Client) Stop() {
 	log.Info("action: graceful_shutdown | result: in_progress")
 
+	c.mu.Lock()
 	c.running = false
-	
+	c.mu.Unlock()
+
+	c.cancel()
+
 	c.cleanup()
-	
+
 	log.Info("action: graceful_shutdown | result: success")
 }
 
 func (c *Client) cleanup() {
 	log.Info("action: cleanup | result: in_progress")
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
 	if c.conn != nil {
 		c.conn.Close()
@@ -91,10 +105,13 @@ func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+		c.mu.Lock()
 		if !c.running {
+			c.mu.Unlock()
 			break
 		}
-		
+		c.mu.Unlock()
+
 		// Create the connection the server in every loop iteration. Send an
 		if err := c.createClientSocket(); err != nil {
 			log.Errorf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
@@ -116,12 +133,19 @@ func (c *Client) StartClientLoop() {
 		c.conn.Close()
 
 		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
+		select {
+		case <-time.After(c.config.LoopPeriod):
+		case <-c.ctx.Done():
+			log.Info("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
+			return
+		}
 	}
 
+	c.mu.Lock()
 	if c.running {
 		log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 	} else {
 		log.Info("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
 	}
+	c.mu.Unlock()
 }
